964e73bf0cccba2c3e2925773153c4014accbc6a
Tyler Engwall <coldfirez@live.com>
2025-05-07 11:39:17 -0500

    added char_count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import num_char
 
 
 def get_book_text(filePath):
@@ -9,7 +10,11 @@ def get_book_text(filePath):
 def main():
     book = get_book_text('books/frankenstein.txt')
     num_words = word_count(book)
+    char_count = num_char(book)
     print(f'{num_words} words found in the document')
+    print(char_count)
+    
+
    
 
 
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_count(book):
     num_words = book.split()
     return len(num_words)
+
+def num_char(book):
+    char_count = {}
+    characters_lower = book.lower()
+    for char in characters_lower:
+        if char in char_count:
+            char_count[char] +=1
+        else:
+            char_count[char] = 1
+    return char_count
